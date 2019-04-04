36709397cf67b382819de31352746420c4ec4baf
Julio <contact@julioespadas.com>
2019-04-04 16:17:42 +0200

    Second activity

---
+++ app/src/main/java/com/julioespadas/myapp/ServiceExActivity.java
@@ -0,0 +1,13 @@
+package com.julioespadas.myapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ServiceExActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_ex);
+    }
+}
