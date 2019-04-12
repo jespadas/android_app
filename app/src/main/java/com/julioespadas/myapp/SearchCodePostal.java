b15933c1bfd94f1c8a03c0a78b7291b4e60eaf73
Julio <contact@julioespadas.com>
2019-04-12 14:29:44 +0200

    api rest

--- app/src/main/java/com/julioespadas/myapp/SearchCodePostal.java
+++ app/src/main/java/com/julioespadas/myapp/SearchCodePostal.java
@@ -1,4 +1,16 @@
 package com.julioespadas.myapp;
 
-public class SearchCodePostal {
+import android.support.v7.app.AppCompatActivity;
+
+import okhttp3.OkHttpClient;
+
+public class SearchCodePostal extends AppCompatActivity {
+
+    public String requestContent(String url) {
+        OkHttpClient okHttpClient = new DefaultHttpClient();
+        Sting result = null;
+
+    }
+
+
 }
